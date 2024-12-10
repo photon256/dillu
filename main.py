128f5297761d70c24aab7d6620dd8db92e5097a5
photon256 <144157217+photon256@users.noreply.github.com>
2024-12-10 21:53:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -194,7 +194,14 @@ async def upload(bot: Client, m: Message):
                       		await bot.send_document(chat_id=m.chat.id, document=filename, caption=cc1)
                             
                       		count += 1
-                            time.sleep(2)
+                            except FloodWait as e:
+                                await m.reply_text(str(e))
+                                time.sleep(1)
+                                continue
+                      
+                      
+                      
+                            
                       	else:
                       		Show = f"**Ｄｏｗｎｌｏａｄｉｎｇ... »**\n\n**Name:{name}**\nQuality » {raw_text2}"
                       		prog = await m.reply_text(Show)
