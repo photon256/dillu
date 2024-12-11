6b82157bdd01bb65b817f9b692904fd8ae7cb69d
photon256 <144157217+photon256@users.noreply.github.com>
2024-12-12 00:08:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -135,8 +135,8 @@ async def upload(bot: Client, m: Message):
         thumb == "no"
 
     
-    
-    await bot.pin_chat_message(m.chat.id, message_id=(f"**Batch Name: {b_name}"))
+    msg = send_message(m.chat.id, (f"**Batch Name: {b_name}"))
+    await bot.pin_chat_message(m.chat.id, message_id=msg.message_id)
     if len(links) == 1:
         count = 1
     else:
