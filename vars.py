2fddfe4576e0c0ca0459415321df34363a70ce53
photon256 <144157217+photon256@users.noreply.github.com>
2024-12-21 19:06:26 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -7,4 +7,4 @@ from os import environ
 API_ID = int(environ.get("API_ID", "22801364"))
 API_HASH = environ.get("API_HASH", "38fd13fcb81f612aa703fa63d7dffec8")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+ADMINS = (os.environ.get("ADMINS", "1996039956").split())
