b52162d32e19bcfc318028415952994ad855d688
photon256 <144157217+photon256@users.noreply.github.com>
2024-12-29 00:37:30 +0530

    Update core.py

--- core.py
+++ core.py
@@ -246,12 +246,12 @@ async def download_video(url,cmd, name):
 
 async def send_doc(bot: Client, m: Message,cc,ka,cc1,prog,count,name):
     reply = await m.reply_text(f"Uploading » `{name}`")
-    time.sleep(1)
+    time.sleep(2)
     start_time = time.time()
     await m.reply_document(ka,caption=cc1)
     count+=1
     await reply.delete (True)
-    time.sleep(1)
+    time.sleep(2)
     os.remove(ka)
     time.sleep(3) 
 
@@ -260,7 +260,7 @@ async def send_vid(bot: Client, m: Message,cc,filename,thumb,name,prog):
     
     subprocess.run(f'ffmpeg -i "{filename}" -ss 00:00:12 -vframes 1 "{filename}.jpg"', shell=True)
     await prog.delete (True)
-    reply = await m.reply_text(f"**Uploading ...** - `{name}`")
+    reply = await m.reply_text(f"<blockquote>**Uploading ...** - `{name}`</blockquote>")
     try:
         if thumb == "no":
             thumbnail = f"{filename}.jpg"
