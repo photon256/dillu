9b2062b9e438d3e10beeaeca20e7bc92251e6562
photon256 <144157217+photon256@users.noreply.github.com>
2024-12-29 23:13:57 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -7,4 +7,5 @@ from os import environ
 API_ID = int(environ.get("API_ID", "22801364"))
 API_HASH = environ.get("API_HASH", "38fd13fcb81f612aa703fa63d7dffec8")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
+OWNER = int(os.environ.get("OWNER", "1996039956"))
 ADMINS = (os.environ.get("ADMINS", "1996039956").split())
