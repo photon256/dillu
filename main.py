ba3f353b3614c694639880c747870061432a465f
photon256 <144157217+photon256@users.noreply.github.com>
2024-12-30 15:25:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import requests
 import subprocess
 
 import core as helper
-from Extractor import bot
+from init import bot
 from utils import progress_bar
 from vars import API_ID, API_HASH, BOT_TOKEN, OWNER
 from aiohttp import ClientSession
