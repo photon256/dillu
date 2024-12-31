184c31e450f8576d00cedeb4fbb1650efabddeb1
photon256 <144157217+photon256@users.noreply.github.com>
2024-12-31 18:57:14 +0530

    Update init.py

--- init.py
+++ init.py
@@ -1,10 +1,10 @@
-import asyncio
+ import asyncio
 import logging
 from pyrogram import Client
 from vars import API_ID, API_HASH, BOT_TOKEN
 
 
-
+loop = asyncio.get_event_loop()
 
 logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
@@ -24,6 +24,6 @@ bot = Client(
 )
 
 
-
+loop.run_until_complete(bot())
 
 
