bfcf5453ad4fa4c02c30aa355e84a2b0854f5988
photon256 <144157217+photon256@users.noreply.github.com>
2025-01-04 10:12:41 +0530

    Update core.py

--- core.py
+++ core.py
@@ -273,9 +273,10 @@ async def send_vid(bot: Client, m: Message,cc,filename,thumb,name,prog):
     dur = int(duration(filename))
 
     start_time = time.time()
-    time.sleep(2)
+    
 
     try:
+        time.sleep(2)
         await m.reply_video(filename,caption=cc, supports_streaming=True,height=720,width=1280,thumb=thumbnail,duration=dur, progress=progress_bar,progress_args=(reply,start_time))
     except Exception:
         await m.reply_document(filename,caption=cc, progress=progress_bar,progress_args=(reply,start_time))
