a0f183f3c31be5979c5772c131e70643811d3fd5
photon256 <144157217+photon256@users.noreply.github.com>
2025-01-04 10:09:02 +0530

    Update core.py

--- core.py
+++ core.py
@@ -273,6 +273,7 @@ async def send_vid(bot: Client, m: Message,cc,filename,thumb,name,prog):
     dur = int(duration(filename))
 
     start_time = time.time()
+    time.sleep(2)
 
     try:
         await m.reply_video(filename,caption=cc, supports_streaming=True,height=720,width=1280,thumb=thumbnail,duration=dur, progress=progress_bar,progress_args=(reply,start_time))
