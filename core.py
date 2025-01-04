9b9db37653e933dc9d8205eba251936346465ee3
photon256 <144157217+photon256@users.noreply.github.com>
2025-01-04 09:57:04 +0530

    Update core.py

--- core.py
+++ core.py
@@ -260,6 +260,7 @@ async def send_vid(bot: Client, m: Message,cc,filename,thumb,name,prog):
     
     subprocess.run(f'ffmpeg -i "{filename}" -ss 00:00:12 -vframes 1 "{filename}.jpg"', shell=True)
     await prog.delete (True)
+    time.sleep(2)
     reply = await m.reply_text(f"**Uploading ...** - `{name}`")
     try:
         if thumb == "no":
