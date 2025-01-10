22cb7aa2d3ae175c292c5d147cde9695f1da3ac6
photon256 <144157217+photon256@users.noreply.github.com>
2025-01-10 20:35:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -245,7 +245,7 @@ async def upload(bot: Client, m: Message):
                       		time.sleep(2)
                       		await helper.send_vid(bot, m, cc, filename, thumb, name, prog)
                       		count += 1
-                            time.sleep(2)
+                        time.sleep(2)
                      except FloodWait as e:
                       await m.reply_text(str(e))
                       time.sleep(2)
