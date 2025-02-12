13b3a9735c40b143757ba42dff68fab562aa9fc3
photon256 <144157217+photon256@users.noreply.github.com>
2025-02-12 21:20:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -178,7 +178,7 @@ async def upload(bot: Client, m: Message):
 
             
             elif 'videos.classplusapp' in url or "tencdn.classplusapp" in url or "webvideos.classplusapp.com" in url or "media-cdn-alisg.classplusapp.com" in url or "videos.classplusapp" in url or "videos.classplusapp.com" in url or "media-cdn-a.classplusapp" in url or "media-cdn.classplusapp" in url:
-                url = f"https://master-api-v2-beah.onrender.com/classp?url={url}&authorization=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMTk5NjAzOTk1NiIsInRnX3VzZXJuYW1lIjoiTm90aGluZyBhcnJ5YSAiLCJpYXQiOjE3MzkzNzIzNTJ9.MiXbUwWHmg1cSTPsvpvsb2TK0n3gdUe-SpI2EBD4orY"
+                url = requests.get(f'https://api.classplusapp.com/cams/uploader/video/jw-signed-url?url={url}', headers={'x-access-token': 'eyJjb3Vyc2VJZCI6IjQ1NjY4NyIsInR1dG9ySWQiOm51bGwsIm9yZ0lkIjo0ODA2MTksImNhdGVnb3J5SWQiOm51bGx9'}).json()['url']
             	
             
             elif "cwmediabkt99.crwilladmin.com" in url:
