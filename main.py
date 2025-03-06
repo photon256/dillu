73506a7844ec99d8b02157998ae360c3731969e1
photon256 <144157217+photon256@users.noreply.github.com>
2025-03-06 09:29:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ async def upload(bot: Client, m: Message):
     update_channel = (-1002363250260)
     chk = await bot.get_chat_member(update_channel, OWNER)
     print(chk)
-    if chk == True:
+    if  "_"== "ChatMember":
         editable = await m.reply_text('<blockquote>Send text</blockquote>')
         input: Message = await bot.listen(editable.chat.id)
         x = await input.download()
